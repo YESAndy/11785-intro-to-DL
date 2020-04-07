ead507ad7f4bdb7e18ffafff9b1daf10221f025c
Cai <35024963+YESAndy@users.noreply.github.com>
2020-04-07 17:17:16 -0400

    Add files via upload

---
+++ hw2/mlp_scan.py
@@ -0,0 +1,127 @@
+# DO NOT import any additional 3rd party external libraries as they will not
+# be available to AutoLab and are not needed (or allowed)
+
+import numpy as np
+import os
+import sys
+
+sys.path.append('mytorch')
+from loss import *
+from activation import *
+from linear import *
+from conv import *
+
+
+class CNN_SimpleScanningMLP():
+    def __init__(self):
+        ## Your code goes here -->
+        # self.conv1 = ???
+        # self.conv2 = ???
+        # self.conv3 = ???
+        # ...
+        # <---------------------
+        self.conv1 = Conv1D(24, 8, 8, 4)
+        self.conv2 = Conv1D(8, 16, 1, 1)
+        self.conv3 = Conv1D(16, 4, 1, 1)
+        self.layers = [self.conv1, self.conv2, self.conv3]
+
+    def __call__(self, x):
+        # Do not modify this method
+        return self.forward(x)
+
+    def init_weights(self, weights):
+        # Load the weights for your CNN from the MLP Weights given
+        # w1, w2, w3 contain the weights for the three layers of the MLP
+        # Load them appropriately into the CNN
+
+        w1, w2, w3 = weights
+
+        self.conv1.W = w1.T.reshape(8, 8, 24).transpose(0, 2, 1)
+        self.conv2.W = w2.T.reshape(16, 8, 1)
+        self.conv3.W = w3.T.reshape(4, 16, 1)
+
+    def forward(self, x):
+        """
+        Do not modify this method
+
+        Argument:
+            x (np.array): (batch size, in channel, in width)
+        Return:
+            out (np.array): (batch size, out channel , out width)
+        """
+
+        out = x
+        for layer in self.layers:
+            out = layer(out)
+        return out
+
+    def backward(self, delta):
+        """
+        Do not modify this method
+
+        Argument:
+            delta (np.array): (batch size, out channel, out width)
+        Return:
+            dx (np.array): (batch size, in channel, in width)
+        """
+
+        for layer in self.layers[::-1]:
+            delta = layer.backward(delta)
+        return delta
+
+
+class CNN_DistributedScanningMLP():
+    def __init__(self):
+        ## Your code goes here -->
+        # self.conv1 = ???
+        # self.conv2 = ???
+        # self.conv3 = ???
+        # ...
+        # <---------------------
+        self.conv1 = Conv1D(24, 2, 2, 2)
+        self.conv2 = Conv1D(2, 8, 2, 2)
+        self.conv3 = Conv1D(8, 4, 2, 1)
+        self.layers = [self.conv1, self.conv2, self.conv3]
+
+    def __call__(self, x):
+        # Do not modify this method
+        return self.forward(x)
+
+    def init_weights(self, weights):
+        # Load the weights for your CNN from the MLP Weights given
+        # w1, w2, w3 contain the weights for the three layers of the MLP
+        # Load them appropriately into the CNN
+
+        w1, w2, w3 = weights
+        self.conv1.W = w1.T.reshape(2, 24, 2).transpose(0, 2, 1)
+        self.conv2.W = w2.T.reshape(8, 2, 2)
+        self.conv3.W = w3.T.reshape(4, 8, 2)
+
+    def forward(self, x):
+        """
+        Do not modify this method
+
+        Argument:
+            x (np.array): (batch size, in channel, in width)
+        Return:
+            out (np.array): (batch size, out channel , out width)
+        """
+
+        out = x
+        for layer in self.layers:
+            out = layer(out)
+        return out
+
+    def backward(self, delta):
+        """
+        Do not modify this method
+
+        Argument:
+            delta (np.array): (batch size, out channel, out width)
+        Return:
+            dx (np.array): (batch size, in channel, in width)
+        """
+
+        for layer in self.layers[::-1]:
+            delta = layer.backward(delta)
+        return delta
